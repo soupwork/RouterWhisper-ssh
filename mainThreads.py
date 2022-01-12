784c14668d5e0247031998d3dce48a29dac26712
soup-works <soupwork@users.noreply.github.com>
2022-01-12 12:42:02 +1100

    cleaning up main
    Main is the first part to set up the program

--- mainThreads.py
+++ mainThreads.py
@@ -1,62 +1,20 @@
 #testing Threads
 #Douglas J. Sheehan
-#2022 Jan 11nd
-#import sys
-#sys.path.insert(1, 'c:/program files/python37/lib/site-packages/')
+#2022 Jan 12nd
 
-from netmiko import ConnectHandler
-from getpass import getpass
+import argparse
 import time
 import logging
 import concurrent.futures
 from viewThreadsCLI import UserPrompts
+from controller import blahblahblah
 
 
-def networkConnection(addy,uname,passwd):
-    
-    
-    ciscorouter = {
-    "device_type": "cisco_ios",
-    "host": addy,
-    "username": uname,
-    "password": passwd,
-        #multiply delay factors by 4
-    "global_delay_factor": 4    
-    }
-
-    net_connect = ConnectHandler(**ciscorouter)
-    
-    net_connect.enable()
-    #
-    show=net_connect.send_command('show ip int brief')
-    #net_connect.send_command('wr mem')
-    net_connect.disconnect()
-    print(f"{show}")
-    return(show)
-    #end NetworkConnection
 
 def main():
-    ipaddylist=['10.94.32.122', '10.94.32.121','10.94.32.120']
-    #ipaddylist=['10.94.32.122']
-    starttime=time.perf_counter()
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        connectdict = {executor.submit(networkConnection, ipaddy, up.username, up.pword):
-                       ipaddy for ipaddy in ipaddylist}
-        for future in concurrent.futures.as_completed(connectdict):
-            print("connect dict IP Addy is ", connectdict[future])
-            dataresult = connectdict[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                print(f"{connectdict[future]} generated exception: {exc}") 
-            else:
-                print(f"{connectdict[future]} \n {data}")       
-
-   
-    endtime=time.perf_counter()    
-    elapsedtime=endtime-starttime
-    print(f"total time is {elapsedtime} seconds")
+    ipaddylist=['192.168.1.1', '192.168.1.1,'192.168.1.1]
+    #ipaddylist=['192.168.1.1]
+    
 if __name__ == '__main__':
     
     up=UserPrompts()
