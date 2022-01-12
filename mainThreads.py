f21f439a7b77222abe77de2ab2c81f262376afa8
soup-works <soupwork@users.noreply.github.com>
2022-01-12 12:43:37 +1100

    Update mainThreads.py

--- mainThreads.py
+++ mainThreads.py
@@ -5,21 +5,18 @@
 import argparse
 import time
 import logging
-import concurrent.futures
 from viewThreadsCLI import UserPrompts
 from controller import blahblahblah
 
 
 
 def main():
+    up=UserPrompts()
+ 
     ipaddylist=['192.168.1.1', '192.168.1.1,'192.168.1.1]
     #ipaddylist=['192.168.1.1]
     
 if __name__ == '__main__':
     
-    up=UserPrompts()
-    #print (up.username, ' is the username before calling main')
-    #print(up.pword, ' is the word before calling main')
-    #x= input ('any key to continue')
-    #up=('PyAdmin', 'password-here')
+    
     main() #call the main method
