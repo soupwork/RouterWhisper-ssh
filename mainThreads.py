8c82ac4651660d3300863c469d5d3ce3645ddec6
soup-works <soupwork@users.noreply.github.com>
2022-01-12 19:27:42 +1100

    Update mainThreads.py

--- mainThreads.py
+++ mainThreads.py
@@ -6,7 +6,7 @@ import argparse
 import time
 import logging
 from viewThreadsCLI import UserPrompts
-from controller import blahblahblah
+from controller-SSH import NetworkController
 
 
 class CLIparams:
@@ -18,8 +18,8 @@ class CLIparams:
             help='specify csv file with host IP addresses to remote to') 
         inputargs.add_argument('-cmd',action='store_true',
             help='specify csv file with commands in order') 
-        inputargs.add_argument('-IP',action='store_true',
-            help='specify a single IP address to connect to') 
+        inputargs.add_argument('-ip',action='store_true',
+            help='specify a single ip address to connect to') 
         
         #inputargs.add_argument('-log', action='store_true', 
         #    help='this will create a log file "Convert7to8_Log_datetime"')
@@ -27,20 +27,37 @@ class CLIparams:
         
 
         inputargs.add_argument('-outfile', action='store',
-            help='This option allowers user to specify a file for saving output.')
+            help='This option allowers user to specify a file for saving output. filename in single quotes')
 
        
 
         cliargs = inputargs.parse_args()
-        self.filename = cliargs.f
+        #self.filename = cliargs.f
+        self.path=cliargs.path
+        print(f"path is {self.path}")
+        self.hosts=cliargs.hosts
+        print(f"hosts is {self.hosts}")
+        self.cmd=cliargs.cmd
+        print(f"cmd {self.cmd}")
+        self.ip=cliargs.ip
+        print(f"single ip is {self.ip}")
+        self.outfile=cliargs.outfile
+        print(f"Outfile is {self.outfile}")
         
 
 def main():
+    """Main sets up the program, and hands over to controller for the rest"""
+    options=CLIparams()
+    print(f"{options} brought in from command line")
     up=UserPrompts()
  
     ipaddylist=['192.168.1.1', '192.168.1.1,'192.168.1.1']
     #ipaddylist=['192.168.1.1]
-    
+    #
+    #  networkJob=NetworkController(up.uname, up.pword, devlist,cmdlist)
+                
+    print("Main Program complete")            
+                
 if __name__ == '__main__':
     """ testing the main program """
     
